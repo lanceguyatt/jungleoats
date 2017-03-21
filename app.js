5f10d979f9acb528e9c1c482c0f70ea2220a55ca
Lance Guyatt <lanceguyatt@gmail.com>
2017-03-21 13:08:55 +0000

    app updates

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const serve = require('serve');
 
 const buildDir = path.resolve(__dirname, 'build');
 
-const server = serve(buildDir, {
+module.exports = serve(buildDir, {
     port: process.env.PORT || 3000,
-    ignore: ['node_modules']
+    ignore: ['node_modules'],
 });
